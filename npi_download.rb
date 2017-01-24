cd26bc984684642643904b355585870c412d6593
Randal Humphries <randal@randalhumphries.com>
2017-01-23 20:27:04 -0500

    Add Comments to the 'NpiDownload' Class

--- npi_download.rb
+++ npi_download.rb
@@ -1,9 +1,9 @@
 require 'net/http'
 require 'json'
-# require 'sparsify'
 
 class NpiDownload
 
+  # API Search Criteria
   attr_accessor :npi_number,
                 :enumeration_type,
                 :taxonomy_description,
@@ -18,38 +18,58 @@ class NpiDownload
                 :limit,
                 :skip
 
+  # Get the Results from the API
   def get_results
-    
+
+    # An internal counter to get the next
+    # set of results from the API
     @result_count = 0
 
+    # An array into which the API results can
+    # be collected
     @results = []
 
+    # Get the first set of results from the API
     json_response = self.query
 
     while true
 
+      # Exit the loop if the API doesn't return
+      # any results and set the "skip" attribute
+      # to nil
       if json_response['result_count'] == 0
         self.skip= nil
         break
       end
 
+      # Add the count of the returned results to the
+      # internal result counter's current value
       @result_count += json_response['result_count']
 
+      # Append the current results to the results
+      # array
       @results << json_response['results']
 
+      # Set the "skip" attribute to the value
+      # on the internal result counter
       self.skip= @result_count
 
+      # Get the next set of results from the API
       json_response = self.query
 
+      # A simple progress bar
       print "#"
 
     end
 
+    # Print the total result count to the console
     puts "\nFound #{@result_count} results."
+
     return @results
 
   end
 
+  # 
   def build_results_hash(results)
 
     @npi_array = []
@@ -105,6 +125,7 @@ class NpiDownload
 
   end
 
+  # Create the URI for the Net::HTTP connection to the API
   def build_uri
 
     URI(
@@ -126,6 +147,8 @@ class NpiDownload
     
   end
 
+  # Connect to the API and Parse the JSON Response into
+  # a Hash
   def query
 
     JSON.parse(Net::HTTP.get(self.build_uri))
